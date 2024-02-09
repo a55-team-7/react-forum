da950911ab0932dc70a6be596a0958e54f7f53ae
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-09 15:41:02 +0200

    Shortyfied the inputs - for better user experience

--- src/components/Register/Register.jsx
+++ src/components/Register/Register.jsx
@@ -1,29 +1,28 @@
 
 import './Register.css'
 
-export default function Register () {
+export default function Register() {
 
     return (
         <div id='register-form'>
-            <h2>Register:</h2>
+            <h3>Join ReadIT and become part of the biggest
+                IT lifestyle forum in the world!</h3>
+            <label htmlFor='register-first-name'>First Name:</label>
+            <input id='register-first-name' type='text' name='register-first-name' />
+            <br />
+            <br />
             <label htmlFor='register-username'>Username:</label>
-            <input id='register-username' type='text' name='register-username'/>
+            <input id='register-username' type='text' name='register-username' />
             <br />
             <br />
             <label htmlFor='register-email'>Email:</label>
-            <input id='register-email' type='text' name='register-email'/>
+            <input id='register-email' type='text' name='register-email' />
             <br />
             <br />
             <label htmlFor='register-password'>Password:</label>
-            <input id='register-password' type='password' name='register-password'/>
+            <input id='register-password' type='password' name='register-password' />
             <br />
             <br />
-            <label htmlFor='register-first-name'>First Name:</label>
-            <input id='register-first-name' type='text' name='register-first-name'/>
-            <br />
-            <br />
-            <label htmlFor='register-last-name'>Last Name:</label>
-            <input id='register-last-name' type='text' name='register-last-name'/>
         </div>
     )
 }
