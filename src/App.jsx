eb1e8612e982e7bae9603e9e45756c9834b98f31
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-09 15:38:06 +0200

    Provide context (not usable for now

--- src/App.jsx
+++ src/App.jsx
@@ -1,25 +1,27 @@
-
-import { BrowserRouter, Navigate, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes } from "react-router-dom";
 import Header from './components/Header/Header';
 import Home from "./components/Home/Home";
 import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 import NotFound from "./components/NotFound/NotFound";
+import AppContext from "./context/AppContext";
 import './App.css'
 
 function App() {
-
+ 
   return (
     <>
     <BrowserRouter>
+    <AppContext.Provider value={{}}> {/* in value we will pass everything the need to share with the other component (not implemented for now)*/}
         <Header />
         <Routes>
-          <Route path="/" element={<Navigate replace to="/home" />} />
-          <Route path="home" element={<Home />} />
-          <Route path="login" element={<Login />} />
-          <Route path="register" element={<Register />} />
+          <Route index element={<Home />} />
+          <Route path="home" element={<Home />} /> {/* also named ReadIT as an experiment ??? home will show different things depending on weather the user is signed in or not*/}
+          <Route path="login" element={<Login />} /> {/* also labeled as Sign In, login would be reusable as we will give the user the option to log in even if he somehow lands on the Register form (Look Excalidraw)*/}
+          <Route path="register" element={<Register />} /> {/* also labeled as Join now , so that no one gets confused */}
           <Route path="*" element={<NotFound />} />
         </Routes>
+      </AppContext.Provider>
     </BrowserRouter>
     </>
   )
