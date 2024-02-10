6e26466f93087fc2d12832a26232afddebede9d0
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-10 09:46:12 +0200

    Improve structure for Home component

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -1,11 +1,25 @@
 
+import Container from '../Container/Container'
+import Popular from '../Popular/Popular'
+import Recents from '../Recents/Recents'
 import './Home.css'
 
-export default function Home () {
+export default function Home() {
     return (
         <>
-        <h2>Current users: 0</h2>
-        <h2>Posts: 0</h2>
+            <div id='currentUsers-and-Post-content'>
+                <Container>
+                <h2 id='number-of-users'>Current users: <br/> 20,109</h2>
+                <h2 id='number-of-posts'>Posts:<br/> 40,290</h2>
+                </Container>
+            </div>
+            <div id='recents-and-popular-content'>
+                <Container>
+                     <Recents /> 
+                    <Popular /> 
+                </Container>
+            </div>
+
         </>
     )
 }
