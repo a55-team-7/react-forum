6166c0a4ba144df2c0492583d5b0b84e6f5ebfb1
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-10 09:40:42 +0200

    Route recents and popular component

--- src/App.jsx
+++ src/App.jsx
@@ -5,26 +5,30 @@ import Register from "./components/Register/Register";
 import Login from "./components/Login/Login";
 import NotFound from "./components/NotFound/NotFound";
 import AppContext from "./context/AppContext";
+import Recents from "./components/Recents/Recents";
 import './App.css'
+import Popular from "./components/Popular/Popular";
 
-function App() {
- 
+const App = () => {
+
   return (
     <>
-    <BrowserRouter>
-    <AppContext.Provider value={{}}> {/* in value we will pass everything the need to share with the other component (not implemented for now)*/}
-        <Header />
-        <Routes>
-          <Route index element={<Home />} />
-          <Route path="home" element={<Home />} /> {/* also named ReadIT as an experiment ??? home will show different things depending on weather the user is signed in or not*/}
-          <Route path="login" element={<Login />} /> {/* also labeled as Sign In, login would be reusable as we will give the user the option to log in even if he somehow lands on the Register form (Look Excalidraw)*/}
-          <Route path="register" element={<Register />} /> {/* also labeled as Join now , so that no one gets confused */}
-          <Route path="*" element={<NotFound />} />
-        </Routes>
-      </AppContext.Provider>
-    </BrowserRouter>
+      <BrowserRouter>
+        <AppContext.Provider value={{}}> {/* in value we will pass everything the need to share with the other component (not implemented for now)*/}
+          <Header />
+          <Routes>
+            <Route index element={<Home />} />
+            <Route path="home" element={<Home />} /> {/* also named ReadIT as an experiment ??? home will show different things depending on weather the user is signed in or not*/}
+            <Route path="login" element={<Login />} /> {/* also labeled as Sign In, login would be reusable as we will give the user the option to log in even if he somehow lands on the Register form (Look Excalidraw)*/}
+            <Route path="register" element={<Register />} /> {/* also labeled as Join now , so that no one gets confused */}
+            <Route path="recents" element={<Recents />} />
+            <Route path="popular" element={<Popular />} />
+            <Route path="*" element={<NotFound />} />
+          </Routes>
+        </AppContext.Provider>
+      </BrowserRouter>
     </>
   )
-}
+};
 
-export default App
+export default App;
