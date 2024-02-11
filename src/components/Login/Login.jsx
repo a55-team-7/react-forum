d4153acfd8c1919911b699fba5ea4c7071555322
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-12 00:03:18 +0200

    Implemented Login component

--- src/components/Login/Login.jsx
+++ src/components/Login/Login.jsx
@@ -1,24 +1,59 @@
-
+import { useNavigate } from 'react-router-dom';
 import Button from '../Button/Button'
+import Container from '../Container/Container'
 import './Login.css'
+import { loginUser } from '../../services/authentication-service';
+import { useState, useContext } from 'react';
+import AppContext from '../../context/AppContext';
+import { useLocation } from 'react-router-dom';
+import { useEffect } from 'react';
 
-export default function Login () {
+
+export default function Login() {
+    const{ user, setContext} = useContext(AppContext);
+    const [form, setForm] = useState({
+        email: '',
+        password: ''
+    })
+
+    const navigate = useNavigate();
+    const location = useLocation();
+
+    const updateForm = prop => e => {
+        setForm({ ...form, [prop]: e.target.value });
+      };
+
+      useEffect(() => {
+        if(user){
+            navigate(location.state?.from || '/home');
+        }
+        }, [user]);
+
+        const login = async () => {
+            try{
+                const credentials = await loginUser(form.email, form.password);
+                setContext({ user: credentials.user, userData: null });
+            }
+            catch(err){
+                console.log(err.message);
+            }
+        }
 
     return (
         <div id='login-form'>
-            <h2>Login:</h2>
-            <label htmlFor='login-username'>Username:</label>
-            <input id='login-username' type='text' name='login-username'/>
-            <br />
-            <br />
-            <label htmlFor='login-email'>Email:</label>
-            <input id='login-email' type='text' name='login-email'/>
-            <br />
-            <br />
-            <label htmlFor='login-password'>Password:</label>
-            <input id='login-password' type='password' name='login-password'/>
+            <Container>
 
-            <Button onClick={() => alert('Test')}>Log In</Button>
+                <h2>Login:</h2>
+                <br />
+                <label htmlFor='login-email'>Email:</label>
+                <input value={form.email} onClick={updateForm('email')} id='login-email' type='text' name='login-email' />
+                <br />
+                <br />
+                <label htmlFor='login-password'>Password:</label>
+                <input value={form.password} onClick={updateForm('password')} id='login-password' type='password' name='login-password' />
+
+            </Container>
+            <Button onClick={login}>Sign In</Button>
         </div>
     )
 }
