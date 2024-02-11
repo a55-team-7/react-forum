73f9a68228bd269129660e22373f0d16ab32a6d9
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-12 00:05:42 +0200

    Create Authenctication component

---
+++ src/hoc/Authenticated.jsx
@@ -0,0 +1,26 @@
+import { useContext } from "react"
+import AppContext from "../context/AppContext"
+import { Navigate, useLocation } from "react-router-dom";
+import PropTypes from 'prop-types';
+
+const Authenticated = ({children}) => {
+    const user = useContext(AppContext);
+    const location = useLocation();
+
+    if(!user){
+        return <Navigate replace to="/login" state={{from: location.pathname}} />
+    }
+
+    return (
+        <>
+            {children}
+        </>
+    );
+}
+
+export default Authenticated;
+
+Authenticated.propTypes = {
+    children: PropTypes.any.isRequired,
+    user: PropTypes.object,
+};
