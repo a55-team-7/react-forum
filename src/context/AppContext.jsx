25b4c3c021de303ad6682ec18b4d063efcfcf1f7
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-12 00:05:21 +0200

    Define the states in the context component

--- src/context/AppContext.jsx
+++ src/context/AppContext.jsx
@@ -3,6 +3,9 @@ import { createContext } from "react";
 const AppContext = createContext({
     //some prop: 'some value'
     //someMethod: () => {}
+    user: null,
+    userData: null,
+    setContext: () => {}
 });
 
 export default AppContext;
