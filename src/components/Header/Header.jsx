d62efe80eba3c4582cceb0525beb944da90ea1f7
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-12 17:54:46 +0200

    Implement login and logout interface

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,16 +1,39 @@
 
-import {NavLink} from 'react-router-dom';
+import { NavLink } from 'react-router-dom';
 import './Header.css';
+import { useContext } from 'react';
+import AppContext from '../../context/AppContext';
+import Button from '../Button/Button';
+import { logoutUser } from '../../services/authentication-service';
 
-export default function Header () {
+export default function Header() {
+    const {user, userData, setContext} = useContext(AppContext);
+
+    const logOut = async () => {
+        await logoutUser();
+        setContext({user: null, userData: null});
+    }
     return (
         <>
-        {/*<h2>How to keep my sanity during Telerik Alpha? Forum</h2>*/}
-        <div className='navigation'>
-            <NavLink to="/home">ReadIT</NavLink>
-            <NavLink to="/login">Sign In</NavLink>
-            <NavLink to="/register">Join now!</NavLink>
-        </div>
+            {/*<h2>How to keep my sanity during Telerik Alpha? Forum</h2>*/}
+            <div className='navigation'>
+                <NavLink to="/home">ReadIT</NavLink>
+
+                {/*if the user is not logged in or logged in*/}
+                {user
+                    ? (
+                        <>
+                            {/* Search Bar */}
+                            {`Welcome, ${userData?.name}`}
+                            {/*profile pic with ptofile component */}
+                            <Button onClick={logOut}>Logout</Button>
+                        </>
+                    )
+                    : (<>
+                        <NavLink to="/register">Register</NavLink>
+                        <NavLink to="/login">Login</NavLink>
+                    </>)}
+            </div>
         </>
     )
 }
