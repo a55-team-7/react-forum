dd5f3ff99ec162b3f36cef5f9d89e972dbd0a179
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-12 17:57:20 +0200

    Implement post service methods

---
+++ src/services/posts-service.js
@@ -0,0 +1,64 @@
+import { get, ref, query, equalTo, orderByChild, update, push } from "firebase/database";
+import { db } from "../config/firebase-setup";
+
+export const addPost = async (author, content, title) => {
+
+    return push(ref(db, 'posts'), {
+        author,
+        title,
+        content,
+        createdOn: Date.now(),
+    });
+
+}
+
+export const getAllPost = async (search) => {   //this function can show all posts by sreation order , but it can also filter posts by title (for now)
+    const snapshot =  await get(query(ref(db, 'posts'), orderByChild('createdOn')));
+
+    if (!snapshot.exists()) {
+        return [];
+    }
+
+    const posts =  Object.keys(snapshot.val().map((key => ({
+        id: key, //the key
+        ...snapshot.val()[key], //console.log  //the key value pairs of this id object
+        createdOn: new Date(snapshot.val()[key].createdOn).toString(),
+    }))))
+    .filter(post => post.title.toLowerCase().includes(search.toLowerCase()));
+
+    return posts; 
+}
+
+export const getPostById = async (id) => {
+
+    const snapshot = await get(ref(db, `posts/${id}`));
+    if (!snapshot.exists()) {
+      return null;
+    }
+  
+    const post = {
+      id,
+      ...snapshot.val(),
+      createdOn: new Date(snapshot.val().createdOn).toString(),
+      likedBy: snapshot.val().likedBy ? Object.keys(snapshot.val().likedBy) : [],
+    };
+  
+    return post;
+  };
+  
+  export const likePost = (handle, postId) => {
+    const updateLikes = {};
+    updateLikes[`/posts/${postId}/likedBy/${handle}`] = true;
+    updateLikes[`/users/${handle}/likedPosts/${postId}`] = true;
+  
+    return update(ref(db), updateLikes);
+  };
+  
+  export const dislikeTweet = (handle, postId) => {
+    const updateLikes = {};
+    updateLikes[`/posts/${postId}/likedBy/${handle}`] = null;
+    updateLikes[`/users/${handle}/likedPost/${postId}`] = null;
+  
+    return update(ref(db), updateLikes);
+  };
+
