189b1bb338757b414e01cb09b19eb7af5cf6b3a3
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-14 22:24:37 +0200

    Implement structure of a post

--- src/components/Post/Post.jsx
+++ src/components/Post/Post.jsx
@@ -1,14 +1,32 @@
 import PropTypes from 'prop-types';
+import Container from '../Container/Container';
+import './Post.css';
+import { useNavigate, useParams } from 'react-router-dom';
 
-const Post = ( {post} ) => {
-
+const Post = ({ post }) => {
+    //const [post, setPost] = useState(null);
+    const navigate = useNavigate();
+   
     return (
-        <div>
-            <h2>Title: {post.title}</h2>
-            <p>Author: {post.author}</p>
-            <p>Content: {post.content}</p>
-            <p>Created on: {post.createdOn}</p>
-            <p>Comments: {post.comments ? Object.values(post.comments).length : 0}</p>
+        <div id="post-container">
+            <Container>
+                {/*author will be displayed with his profile pic and next to him - his name/username*/}
+                <div id="post-header-container">
+                    <p>*photo* {post.author}</p>
+                    <button onClick={() => navigate(`/posts/${post.id}`)} >see more</button>
+                </div>
+                <h4>{post.title}</h4>
+                {/* content needt to be in a container*/}
+                <div id="content-container">
+                    <p >Content: {post.content}</p>
+                </div>
+                {/*Created on only for the detailed Post view */}
+                {/*<p>Created on: {post.createdOn}</p>*/}
+                {/*likes and comments need to be in a seperaye grid */}
+                <div id="likes-comments-container">
+                    <p> Likes: {post.likes ? Object.values(post.likes).length : 0}  Comments: {post.comments ? Object.values(post.comments).length : 0}</p>
+                </div>
+            </Container>
         </div>
     )
 };
