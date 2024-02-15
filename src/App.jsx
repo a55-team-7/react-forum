7b634603d73346524fb2efbb40739a85edc313c8
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-15 21:18:16 +0200

    Implemented search bar  globally

--- src/App.jsx
+++ src/App.jsx
@@ -1,4 +1,4 @@
-import { BrowserRouter, Route, Routes } from "react-router-dom";
+import { BrowserRouter, Route, Routes, useSearchParams } from "react-router-dom";
 import Header from './components/Header/Header';
 import Home from "./components/Home/Home";
 import Register from "./components/Register/Register";
@@ -17,17 +17,44 @@ import PostDetails from "./components/Post Details/PostDetails";
 import UserPage from "./components/UserPage/UserPage";
 import AllPosts from "./components/AllPosts/AllPosts";
 
+const AllRoutes = () => {
+  const [searchParams, setSearchParams] = useSearchParams();
+  const search = searchParams.get('search') || '';
+
+  const setSearch = (value) => {
+    setSearchParams({ search: value });
+  }
+
+  return (
+    <>
+      <Header search={search} setSearch={setSearch} />
+      <Routes>
+        <Route index element={<Home />} />
+        <Route path="home" element={<Home />}>
+          <Route path="recents" element={<Recents />} />
+          <Route path="popular" element={<Popular />} />
+          <Route path="create-post" element={<CreatePost />} />
+          <Route path="my-posts" element={<AllPosts search={search}/>} />
+          <Route path="posts/:id" element={<PostDetails />} />
+        </Route>
+        <Route path="login" element={<Login />} />
+        <Route path="register" element={<Register />} />
+        <Route path="users/:handle" element={<UserPage />} />
+        <Route path="*" element={<NotFound />} />
+      </Routes>
+    </>
+  );
+}
+
 const App = () => {
   const [context, setContext] = useState({
     user: null,
     userData: null,
-  }) //we will use this to share data between components [not implemented for now
+  })
 
-  const [user, loading, error] = useAuthState(auth); //this is a hook from firebase that will return the user, loading and error
-
+  const [user, loading, error] = useAuthState(auth);
   useEffect(() => {
     if (user) {
-
       getUserData(user.uid)
         .then(snapshot => {
           if (snapshot.exists()) {
@@ -35,39 +62,14 @@ const App = () => {
           }
         })
     }
-  }, [user, loading, error]) //this will update the context when  the user changes
+  }, [user, loading, error])
 
   return (
-    <>
-      <BrowserRouter>
-        <AppContext.Provider value={{ ...context, setContext }}> {/* in value we will pass everything the need to share with the other component (not implemented for now)*/}
-          <Header />
-          <Routes>
-            <Route index element={<Home />} />
-            <Route path="home" element={<Home />} > {/* also named ReadIT as an experiment ??? home will show different things depending on weather the user is signed in or not*/}
-              <Route path="recents" element={<Recents />} />
-              <Route path="popular" element={<Popular />} />
-              <Route path="create-post" element={<CreatePost />} />
-              <Route path="my-posts" element={<AllPosts />} />
-              <Route path="posts/:id" element={<PostDetails />} />
-            </Route>
-            <Route path="login" element={<Login />} /> {/* also labeled as Sign In, login would be reusable as we will give the user the option to log in even if he somehow lands on the Register form (Look Excalidraw)*/}
-            <Route path="register" element={<Register />} /> {/* also labeled as Join now , so that no one gets confused */}
-            {/* Authenticated content:
-             AllPost(or Feed) ,
-              My Uploads,
-               Post Details, Create Posts , 
-               Settings,
-               Profile,
-               Logout???*/}
-
-            <Route path="users/:handle" element={<UserPage />} />
-            
-            <Route path="*" element={<NotFound />} />
-          </Routes>
-        </AppContext.Provider>
-      </BrowserRouter>
-    </>
+    <BrowserRouter>
+      <AppContext.Provider value={{ ...context, setContext }}>
+        <AllRoutes />
+      </AppContext.Provider>
+    </BrowserRouter>
   )
 };
 
