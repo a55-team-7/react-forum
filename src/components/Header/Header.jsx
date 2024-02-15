4035558ff6fc46f9a91608411e1607a03da3b95c
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-15 21:18:52 +0200

    Validate props

--- src/components/Header/Header.jsx
+++ src/components/Header/Header.jsx
@@ -1,47 +1,52 @@
-
 import { NavLink } from 'react-router-dom';
 import './Header.css';
 import { useContext } from 'react';
 import AppContext from '../../context/AppContext';
 import Button from '../Button/Button';
 import { logoutUser } from '../../services/authentication-service';
+import PropTypes from 'prop-types';
 
-export default function Header() {
-    const {user, userData, setContext} = useContext(AppContext);
+export default function Header({ search, setSearch }) {
+    const { user, userData, setContext } = useContext(AppContext);
 
     const logOut = async () => {
         await logoutUser();
-        setContext({user: null, userData: null});
+        setContext({ user: null, userData: null });
     }
 
     let userPageLink = '/users/';
     if (userData) {
         userPageLink += userData.handle;
     }
-    
+
     return (
         <>
-            {/*<h2>How to keep my sanity during Telerik Alpha? Forum</h2>*/}
             <div className='navigation'>
                 <NavLink to="/home">ReadIT</NavLink>
-                {/*<NavLink to="/posts/-NqWtfEVY4vVVMz82z2A">Test Post Details</NavLink>*/}
 
-                {/*if the user is not logged in or logged in*/}
                 {user
                     ? (
                         <>
-                            {/* Search Bar */}
-                            {`Welcome, ${userData?.firstName}`}
-                            {/*profile pic with ptofile component */}
+                            <input value={search} onChange={e => setSearch(e.target.value)} type="text" name="search" id="search" />
+                            {search && <Button onClick={() => setSearch('')}>Clear</Button>}
                             <Button onClick={logOut}>Logout</Button>
                             <NavLink to={userPageLink}>User Page</NavLink>
                         </>
                     )
-                    : (<>
-                        <NavLink to="/register">Register</NavLink>
-                        <NavLink to="/login">Login</NavLink>
-                    </>)}
+                    : (
+                        <>
+                            <NavLink to="/register">Register</NavLink>
+                            <NavLink to="/login">Login</NavLink>
+                        </>
+                    )
+                }
             </div>
         </>
     )
+}
+
+
+Header.propTypes = {
+    search: PropTypes.string,
+    setSearch: PropTypes.func
 }
