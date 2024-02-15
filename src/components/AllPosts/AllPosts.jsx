8da579cd2de4a217670a135d851c5dd96c1109c3
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-15 21:18:36 +0200

    Filter posts by search

--- src/components/AllPosts/AllPosts.jsx
+++ src/components/AllPosts/AllPosts.jsx
@@ -3,8 +3,9 @@ import { useEffect, useState } from 'react';
 import { getAllPost } from '../../services/posts-service';
 //import Container from '../Container/Container';
 //import './AllPosts.css';
+import PropTypes from 'prop-types';
 
-const AllPosts = () => {
+const AllPosts = ({ search }) => {
   const [posts, setPosts] = useState([]);
 
   useEffect(() => {
@@ -13,22 +14,27 @@ const AllPosts = () => {
       setPosts(posts);
     }
     fetchPosts()
-  }, [posts]); //when a post is deleted or edited for example we need to refetch
+  }, [search]); // refetch when search changes
+
+  const filteredPosts = posts.filter(post => 
+    post.title.toLowerCase().includes(search.toLowerCase())
+  );
 
   return (
     <div>
       <h1>My feed</h1>
       <div id="posts-container">
-
-          <div>{posts.map(post => (
+          <div>{filteredPosts.map(post => (
             <Post key={post.id} post={post} />
           ))}
           </div>
-      
       </div>
     </div>
   );
-
 };
 
 export default AllPosts;
+
+AllPosts.propTypes = {
+  search: PropTypes.string
+}
