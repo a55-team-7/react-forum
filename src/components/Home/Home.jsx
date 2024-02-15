84cb525842c91521fbf5f5e772585ca4ec838a9c
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-15 18:33:45 +0200

    Implement nested routing and putting components in differents containers for easier styling

--- src/components/Home/Home.jsx
+++ src/components/Home/Home.jsx
@@ -8,44 +8,50 @@ import AppContext from '../../context/AppContext'
 import { Link, Outlet } from 'react-router-dom'
 
 export default function Home() {
-    const {user} = useContext(AppContext);
-    const [view, setView]= useState('home');
+    const { user } = useContext(AppContext);
+    const [view, setView] = useState('home');
 
     return (
         <>
-        {user ?
-            <div>
-                <div id='sidebar-content'>
-                    <Container>
-                    <Link to="/recents">Recents</Link>
-                    <Link to="/popular">Popular</Link>
-                    <Link to="/my-posts">My Posts</Link>
-                    <Link to="/create-post">Create Post</Link>
+            {user ?
+                <div>
+                    <div id="home-layout-grid-authenticated">
+                        <div id='sidebar-content'>
+                            <Container>
+                                <Link to="recents">Recents</Link>
+                                <Link to="popular">Popular</Link>
+                                <Link to="my-posts">My Posts</Link>
+                                <Link to="create-post">Create Post</Link>
+                                <Link to="settings">Settings</Link>
+                            </Container>
+                        </div>
+
+                        <div id='main-content'>
+                            <Container>
+                                <Outlet />
+                            </Container>
+                        </div>
                         
-                        {/*<MyPosts />*/}
-                        {/*<CreatePost />*/}
-                        {/*</*Settings />*/}
-                    </Container>
+                    </div>
                 </div>
-            </div>
-            :
-            <div>
-                <div id='currentUsers-and-Post-content'>
-                    <Container>
-                        <h2 id='number-of-users'>Current users: <br/> 20,109</h2>
-                        <h2 id='number-of-posts'>Posts:<br/> 40,290</h2>
-                    </Container>
+                :
+                <div>
+                    <div id='currentUsers-and-Post-content'>
+                        <Container>
+                            <h2 id='number-of-users'>Current users: <br /> 20,109</h2>
+                            <h2 id='number-of-posts'>Posts:<br /> 40,290</h2>
+                        </Container>
+                    </div>
+                    <div id='recents-and-popular-content'>
+                        <Container>
+                            <button onClick={() => setView('recents')}>Recents</button>
+                            <button onClick={() => setView('popular')}>Popular</button>
+                            {view === 'recents' && <Recents />}
+                            {view === 'popular' && <Popular />}
+                        </Container>
+                    </div>
                 </div>
-                <div id='recents-and-popular-content'>
-                    <Container>
-                    <button onClick={() => setView('recents')}>Recents</button>
-                    <button onClick={() => setView('popular')}>Popular</button>
-                        {view === 'recents' && <Recents />}
-                        {view === 'popular' && <Popular />}
-                    </Container>
-                </div>
-            </div>
-        }
+            }
 
         </>
     )
