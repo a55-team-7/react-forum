5dc7af83ff8ef9cd9af5be0e07e958cde1be7db7
Vyara Delcheva <valentinesbyv@yahoo.com>
2024-02-16 13:38:49 +0200

    Make admin view and options/ block/ unblock/ edit for your own posts

--- src/components/UserPage/UserPage.jsx
+++ src/components/UserPage/UserPage.jsx
@@ -2,21 +2,39 @@ import { useEffect, useState, useContext } from "react";
 import AppContext from "../../context/AppContext";
 import { useParams } from "react-router-dom"
 import './UserPage.css'
-import { getUserByHandle, uploadProfilePictureByHandle } from "../../services/users-service";
+import { getUserByHandle, uploadProfilePictureByHandle, updateUserByHandle, unblockUser, blockUser, makeUserAdmin } from "../../services/users-service";
 import Button from "../Button/Button";
 import Post from "../Post/Post";
 import { getAllPost } from "../../services/posts-service";
 import ProfilePicture from "../ProfilePicture/ProfilePicture";
 
-
 export default function UserPage() {
+
+    //update the local user 
     const [user, setUser] = useState(null);
+
+    //updates of the posts 
     const [allPosts, setAllPosts] = useState([]);
+
+    //profile pics updates
     const [profilePicture, setProfilePicture] = useState(null);
     const [yourOwnProfile, setYourOwnProfile] = useState(false);
+
+    //user the data of our user
     const { userData } = useContext(AppContext);
+
+    //updating the block status
+    const [isBlocked, setIsBlocked] = useState(false);
+
+    //taking the handle from the url to know which user we're looking at
     const { handle } = useParams();
 
+    //update the edit view
+    const [isEditing, setIsEditing] = useState(false);
+    const [updatedUser, setUpdatedUser] = useState({ firstName: '', lastName: '', email: '', isBlocked: false });
+    //
+
+   
 
     useEffect(() => {
         if (userData && userData.handle === handle) {
@@ -27,15 +45,15 @@ export default function UserPage() {
 
     useEffect(() => {
         getAllPost().then(setAllPosts);
-    }, [allPosts]);
+    }, []); //removed allPosts as a dependencybecause it can cause infinite loop
 
     const updateProfilePicture = (event) => {
         setProfilePicture(event.target.files[0]);
     }
 
-    const uploadProfilePicture = () => {
+    const uploadProfilePicture = async () => {
         if (profilePicture) {
-            uploadProfilePictureByHandle(handle, profilePicture);
+            await uploadProfilePictureByHandle(handle, profilePicture);
         } else {
             alert('please upload a file');
         }
@@ -43,14 +61,63 @@ export default function UserPage() {
 
     const userPosts = (user && allPosts) ? allPosts.filter(post => post.author === user.handle) : [];
 
+
+    //here we start setting the values , enabling the edit mode
+    const startEditing = () => {
+        setUpdatedUser({ firstName: user.firstName, lastName: user.lastName, email: user.email });
+        setIsEditing(true);
+    }
+
+    //here we update the user in the database
+    const saveChanges = async () => {
+        await updateUserByHandle(handle, updatedUser);
+        setIsEditing(false);
+        getUserByHandle(handle).then(setUser);
+    }
+    
+    const toggleBlockUser = async () => {
+        if (isBlocked) {
+            await unblockUser(user.uid);
+        } else {
+            await blockUser(user.uid);
+        }
+        setIsBlocked(!isBlocked);
+        getUserByHandle(handle).then(setUser);
+    };
+
+    const giveAdminRights = async () => {
+        await makeUserAdmin(handle);
+        getUserByHandle(handle).then(setUser);
+    }
+
     return (
         <div id='user-page'>
             {(user) ? (
                 <>
-
                     <h2>User: {user.handle}</h2>
-                    <h3>Name: {user.firstName} {user.lastName}</h3>
-                    <h3>Email: {user.email}</h3>
+
+                    {/*if profile is his own*/}
+                    {isEditing ? (
+                        <>
+                            <input value={updatedUser.firstName} onChange={e => setUpdatedUser({ ...updatedUser, firstName: e.target.value })} />
+                            <input value={updatedUser.lastName} onChange={e => setUpdatedUser({ ...updatedUser, lastName: e.target.value })} />
+                            <input value={updatedUser.email} onChange={e => setUpdatedUser({ ...updatedUser, email: e.target.value })} />
+                            <Button onClick={saveChanges}>Save</Button>
+                        </>
+                    ) : (
+                        <>
+                            <h3>Name: {user.firstName} {user.lastName}</h3>
+                            <h3>Email: {user.email}</h3>
+
+                            {/*if profile is  his own*/}
+                            {yourOwnProfile && <Button onClick={startEditing}>Edit</Button>}
+                        </>
+                    )}
+
+                    {/*if the user is an admin and the profile is not his own*/}
+                    {userData && userData.isAdmin &&  userData.handle !== handle && !user.isAdmin && <Button onClick={toggleBlockUser}>{isBlocked ? 'Unblock user' : 'Block user'}</Button>}
+                    {userData && userData.isAdmin &&  userData.handle !== handle && !user.isAdmin && <Button onClick={giveAdminRights}>Make admin</Button>}
+
                     <ProfilePicture handle={handle}/>
                     {yourOwnProfile && (
                         <div id='upload-picture'>
