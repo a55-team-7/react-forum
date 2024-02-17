f1a566e408282d63fa59b4d441623556cd5947ab
Yordan <yordan.blagov@abv.bg>
2024-02-18 00:56:52 +0200

    Popular and Recents (implemented partially)

--- src/components/Popular/Popular.jsx
+++ src/components/Popular/Popular.jsx
@@ -1,9 +1,25 @@
+import { useEffect, useState } from 'react';
+import { getPostsByMostLikes } from '../../services/posts-service';
+import Post from '../Post/Post';
+import { MAX_POPULAR_POSTS } from '../../common/contants';
+
 const Popular = () => {
-    return (
-        <>
-        <h5 id="popular">Popular</h5>
-        </>
-    );
+  const [posts, setPosts] = useState([]);
+
+  useEffect(() => {
+    getPostsByMostLikes()
+      .then(posts => setPosts(posts.slice(0, MAX_POPULAR_POSTS)))
+      .catch(console.error);
+  }, []);
+
+  return (
+    <div>
+      <h2>Popular Posts</h2>
+      {posts.map((post, index) => (
+        <Post key={index} post={post} />
+      ))}
+    </div>
+  );
 };
 
 export default Popular;
